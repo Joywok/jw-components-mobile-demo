cab7af95a093578f6a502ac1494467dfa6902f38
zhongshanshan <zhongshsh@dogesoft.cn>
2017-11-20 19:55:38 +0800

    fixed code

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,49 +1,29 @@
-import { Modal, Button, WhiteSpace, WingBlank, Toast } from 'antd-mobile';
+import { Button, WingBlank, WhiteSpace } from 'antd-mobile';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AlertBase } from '../common/modal';
 
-const alert = Modal.alert;
-const prompt = Modal.prompt;
-
-
-const showAlert = () => {
-  const alertInstance = alert('Delete', 'Are you sure???', [
-    { text: 'Cancel', onPress: () => console.log('cancel'), style: 'default' },
-    { text: 'OK', onPress: () => console.log('ok') },
-  ]);
-  setTimeout(() => {
-    // 可以调用close方法以在外部close
-    console.log('auto close');
-    alertInstance.close();
-  }, 500000);
-};
-
-const showPrompt = () => {
-  const alertPrompt = prompt(<img src="../button/images/123.png" />,"请输入您的名字",
-    [
-      {
-        text: '确认',
-        onPress: value => new Promise((resolve) => {
-          Toast.info('onPress promise', 1);
-          setTimeout(() => {
-            resolve();
-            console.log(`value:${value}`);
-          }, 1000);
-        }),
+const alertModal = () =>{
+  AlertBase({
+      tip: '已成功提交',
+      icon: 'icon-alert-success',
+      onOk: ()=>{ // 点击确认回调
+        console.log('onOk')
       },
-    ], 'default', null, ['请输入您的名字'])
+      onClose: ()=>{ // 关闭弹框回调
+
+      },
+      okBtn: {   // 此参数可不传，不传默认为 知道了
+        text: '确定'
+      }
+    });
 }
-
-
 
 const App = () => (
   <WingBlank size="lg">
 
     <WhiteSpace size="lg" />
-    <Button className="modal-alert" onClick={showAlert}>confirm</Button>
-
-    <WhiteSpace size="lg" />
-    <Button className="modal-prompt" onClick={showPrompt}>promise</Button>
+    <Button className="modal-alert" onClick={alertModal}>Alert Modal</Button>
 
   </WingBlank>
 );
